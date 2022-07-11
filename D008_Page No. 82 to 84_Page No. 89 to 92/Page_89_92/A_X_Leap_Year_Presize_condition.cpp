725ea51920d313d17dd1e4e9aad926e776d492d2
aneeketj_office <aneeket.jangam@gmail.com>
2022-07-11 16:55:24 +0530

    trying A

---
+++ D008_Page No. 82 to 84_Page No. 89 to 92/Page_89_92/A_X_Leap_Year_Presize_condition.cpp
@@ -0,0 +1,30 @@
+//(a) 	Any year is entered through the keyboard, write a program to
+//	determine whether the year is leap or not. Use the logical
+//	operators && and ||.
+
+
+// Any year that is evenly divisible by 4 is a leap year.
+// However, there is still a small error that must be accounted for. 
+// To eliminate this error, the Gregorian calendar stipulates that 
+// a year that is evenly divisible by 100 (for example, 1900) 
+// is a leap year only if it is also evenly divisible by 400.
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int yr, f, h, fh;
+
+    cout << endl << "Enter the year to check in YYYY format:\t";
+    cin >> yr;
+
+    if (((yr%4 == 0) && (yr != 100)) || ((yr%4 == 0) && (yr%100 == 0) && (yr%400 == 0)))
+        cout << endl << "The entered year is a leap year";
+    else
+        cout << endl << "The entered year is NOT a leap year";
+    
+    return 0;
+    
+}
