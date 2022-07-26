89a25e001d1ba2f09cac969b280a04669718f3e8
aneeketj <aneeket.jangam@gmail.com>
2022-07-26 22:30:56 +0530

    D018_Assignment

---
+++ D018_22_July_User_Defined_Functions/max_min_in_array_udf.cpp
@@ -0,0 +1,21 @@
+//max and min array,
+
+#include<iostream>
+using namespace std;
+
+int max(int max);
+int min(int min);
+
+int main()
+{
+    int a[5], i;
+
+    for (i=0; i<5; i++)
+    {
+        cout << endl << "Enter element in array:\t";
+        cin >> a[i];
+    }
+    
+    
+    
+}
---
+++ D018_22_July_User_Defined_Functions/reverse_UDF.cpp
@@ -0,0 +1,37 @@
+//specially for ariyanta try to find reverse using udf.
+
+
+#include<iostream>
+using namespace std;
+int reverse(int);
+int main()
+{
+    int num, rnumber;
+
+    cout << endl << "Enter the number:\t";
+    cin>>num;
+
+    rnumber=reverse(num);
+
+    cout<<endl<<"REVERSE Number is\t"<<rnumber;
+
+    return 0;
+}
+
+int reverse(int n)
+{
+    
+    int rem, rnumber=0;
+
+    while(n>0)
+    {
+        rem=n%10;
+
+        rnumber=(rnumber*10)+rem;
+
+        n=n/10;
+    }
+
+    return rnumber;
+
+}
