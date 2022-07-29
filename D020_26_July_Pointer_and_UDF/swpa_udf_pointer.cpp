5774885814c04aecbbec27c7bf54c78552f8c8b3
aneeketj <aneeket.jangam@gmail.com>
2022-07-29 19:28:03 +0530

    just some updates

---
+++ D020_26_July_Pointer_and_UDF/swpa_udf_pointer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *, int )
+
+int main()
+{
+    int a,b;
+    cout << endl << "Enter A:\t";
+    cin >> a;
+
+    cout << endl << "Enter B:\t";
+    cin >> b;
+
+    swap(a,b);
+    cout << endl << "A is\t" <<a;
+    cout << endl <<
+}
